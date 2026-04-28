e599414814a1927fec943586da4a706974cbdd31
rickyrMan72 <aquinogaona@hotmail.com>
2026-04-29 00:08:41 +0200

    Add files via upload

--- js/api/ejercicios.api.js
+++ js/api/ejercicios.api.js
@@ -1,1 +1,25 @@
+import { db, collection, addDoc, onSnapshot, deleteDoc, doc, updateDoc } from '../firebase-config.js';
 
+export const ejerciciosApi = {
+    suscribir: (callback) => {
+        return onSnapshot(collection(db, 'ejercicios'), (snapshot) => {
+            const ejercicios = [];
+            snapshot.forEach((doc) => ejercicios.push({ id: doc.id, ...doc.data() }));
+            callback(ejercicios);
+        }, (error) => {
+            console.error("Error fetching exercises:", error);
+        });
+    },
+
+    crear: async (datos) => {
+        return await addDoc(collection(db, 'ejercicios'), datos);
+    },
+
+    actualizar: async (id, datos) => {
+        return await updateDoc(doc(db, 'ejercicios', id), datos);
+    },
+
+    eliminar: async (id) => {
+        return await deleteDoc(doc(db, 'ejercicios', id));
+    }
+};
